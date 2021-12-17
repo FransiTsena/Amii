ca4704fe14a084fda631fd4f0c6f06275dbd94ca
FransiTsena <94781526+FransiTsena@users.noreply.github.com>
2021-12-17 20:28:01 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,10 +20,10 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "icons/logo192.png": "3db8623127d4f0172c98c69060152ede",
 "icons/logo512.png": "1b87266c3bcf608a2e50e99aacae1c18",
-"index.html": "6284047b8dba527c0318edfe129c342c",
-"/": "6284047b8dba527c0318edfe129c342c",
+"index.html": "384075b28a7725023fce14efff750cd6",
+"/": "384075b28a7725023fce14efff750cd6",
 "logo.png": "6da1f83be5bb24d15de23e3fd3eda092",
-"main.dart.js": "02b481ccf22c77f3dd5cca1b946e8b29",
+"main.dart.js": "948aeb90fb2137799b8bff6001417372",
 "manifest.json": "527032506bdc207035ab141e3edee550",
 "version.json": "0cfc9526247eb316d8c9493e624b6d36"
 };
